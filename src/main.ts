8c75b7e8c975f2e3405a7647e06944522fca268e
aceassured <venkatatrinadh4444@gmail.com>
2025-12-30 16:08:00 +0530

    Added the new origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["*", "http://localhost:5173",],
+    origin: ["*", "http://localhost:5173","http://localhost:3000"],
     credentials: true
   }),
     app.useGlobalPipes(new ValidationPipe({
